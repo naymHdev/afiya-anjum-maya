9e20b32173ace668f5b97cf299175695e8353a0f
MacBook Pro <naym100m@gmail.com>
2024-07-10 17:44:12 +0600

    create : a newslatter section

--- src/components/NewsLatter/index.jsx
+++ src/components/NewsLatter/index.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import { HiOutlineCursorClick } from "react-icons/hi";
+import React from "react";
+import { RxArrowTopRight } from "react-icons/rx";
 
 const NewsLatter = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <div className=" px-5 md:px-6 lg:px-10 mt-16 md:mt-24">
+        <div className=" grid grid-cols-1 md:grid-cols-7 gap-5">
+          <div className=" relative col-span-full md:col-span-2">
+            <div className=" h-32 w-32 bg-custom-gradient absolute top-0 right-0 shadow-2xl shadow-orange-100 rounded-t-2xl rounded-bl-full"></div>
+            <div className=" shadow-2xl rounded-2xl px-6 py-10">
+              <div className=" px-5">
+                <HiOutlineCursorClick className=" text-8xl text-orange-400" />
+              </div>
+              <h2 className=" mt-8 text-2xl font-semibold text-primary-text">
+                Pay Per Click
+              </h2>
+              <p className=" mt-4 text-sm text-secondary-text font-medium">
+                We help businesses bring ideas to life in the digital world
+                design ing & implementing...
+              </p>
+              <div className=" mt-10">
+                <button className="flex items-center gap-1 text-sm font-medium">
+                  READ MORE <RxArrowTopRight />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className=" col-span-full md:col-span-5">
+            <div className=" border rounded-2xl h-full shadow-md">
+              <div className=" mt-20 w-full mx-auto">
+                <h2 className=" text-3xl font-bold text-black text-center">
+                  Want to know your SEO Score?
+                </h2>
+                <div className="bg-white rounded-full flex items-center justify-between p-2 shadow-lg max-w-3xl mx-auto w-10/12 mt-8">
+                  <input
+                    type="text"
+                    placeholder="Your Website URL"
+                    className="flex-grow p-4 text-gray-700 rounded-l-full focus:outline-none"
+                  />{" "}
+                  |
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    className="flex-grow p-4 text-gray-700 focus:outline-none"
+                  />
+                  <button className="bg-red-500 text-white py-2 px-6 rounded-full flex items-center justify-center hover:bg-red-600 transition-colors">
+                    Start Now
+                    <svg
+                      className="w-6 h-6 ml-2"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M5 12h14M12 5l7 7-7 7"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default NewsLatter;
