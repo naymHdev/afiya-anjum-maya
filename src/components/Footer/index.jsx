2ae81b8de48d795e1631d174951361c93179cc40
MacBook Pro <naym100m@gmail.com>
2024-07-11 16:19:01 +0600

    compleate: footer subscription section

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,12 +6,14 @@ import {
   FaPinterest,
 } from "react-icons/fa";
 import { MdCall, MdLocationPin, MdEmail } from "react-icons/md";
+import { TfiArrowTopRight } from "react-icons/tfi";
 import Wave from "react-wavify";
+import "./footer.css";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="relative bg-purple-800 text-white py-10 mt-28">
+    <footer className="relative bg-gradient-to-r from-[#31077D] to-[#6340C0] text-white py-10 mt-28">
       <div className="absolute inset-x-0 -top-20 hidden">
         <Wave
           fill="#6B21A8"
@@ -27,7 +29,7 @@ const Footer = () => {
       </div>
 
       <section className=" px-4 md:px-6 lg:px-10">
-        <div className=" grid grid-cols-1 md:grid-cols-3 gap-5">
+        <div className=" grid grid-cols-1 md:grid-cols-3 gap-8">
           {/* Fist section  || in address section */}
           <div>
             <div className="mb-6 lg:mb-0 space-y-4">
@@ -89,50 +91,50 @@ const Footer = () => {
           {/* Second section  || in menus section */}
           <div className=" flex justify-between">
             <div>
-              <h2 className="text-xl font-bold mb-2">Menu</h2>
-              <ul>
+              {/* <h2 className="text-xl font-bold mb-2">Menu</h2> */}
+              <ul className="space-y-3 font-medium">
                 <li>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="menu-item">
                     About Us
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="menu-item">
                     Services
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="menu-item">
                     Blog
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="menu-item">
                     Pricing
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="menu-item">
                     Contact Us
                   </a>
                 </li>
               </ul>
             </div>
             <div>
-              <h2 className="text-xl font-bold mb-2">Quick Links</h2>
-              <ul>
+              {/* <h2 className="text-xl font-bold mb-2">Quick Links</h2> */}
+              <ul className=" space-y-3 font-medium">
                 <li>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="menu-item">
                     Privacy Policy
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="menu-item">
                     Terms & Conditions
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="menu-item">
                     Cookie Policy
                   </a>
                 </li>
@@ -142,27 +144,32 @@ const Footer = () => {
 
           {/* Third section  || in subscription section */}
           <div>
-            <h2 className="text-xl font-bold mb-2">
+            <h2 className="text-4xl font-medium mb-8">
               Subscribe to Our Newsletter
             </h2>
-            <p className="mb-4">
+            <p className="mb-4 text-sm font-medium text-gray-200">
               Stay up-to-date with the latest trends in digital marketing and
               receive exclusive tips and insights by subscribing to our
               newsletter.
             </p>
+
+            {/* Newsletter form */}
             <form>
-              <div className="flex">
-                <input
-                  type="email"
-                  placeholder="Enter your email"
-                  className="p-2 rounded-l-md focus:outline-none text-gray-700"
-                />
-                <button
-                  type="submit"
-                  className="bg-orange-400 text-white px-4 py-2 rounded-r-md hover:bg-orange-500 transition-colors"
-                >
-                  Subscribe
-                </button>
+              <div className="flex relative border rounded-xl">
+                <div>
+                  <input
+                    className=" px-5 py-7 focus:outline-none border-none bg-transparent text-white"
+                    type="email"
+                    name=""
+                    id=""
+                    placeholder="Enter your email"
+                  />
+                </div>
+                <div className=" absolute top-4 right-2">
+                  <button className=" text-white bg-custom-gradient flex items-center gap-3 font-medium rounded-xl px-4 py-3">
+                    Subscribe <TfiArrowTopRight size={16} />
+                  </button>
+                </div>
               </div>
             </form>
           </div>
